dc11897836ed049f5d8e0cd91f61f5a65dfe286f
Phi Hoang <hoangphi.dev@gmail.com>
2020-08-04 22:27:09 +0700

    update multi array

---
+++ multi.php
@@ -0,0 +1,16 @@
+<?php 
+	$data = array(
+		array(
+			'title' => "tesst",
+			'content' => "sky tesst"
+		),
+		"Hello",
+		"Phi",
+		22
+	);
+
+	echo '<pre>';
+	var_dump($data[1]);
+	echo '<pre>';
+	// echo json_encode($data);
+ ?>
