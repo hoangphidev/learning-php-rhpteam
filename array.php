843899151a55e567c10018e1f86a84dc2134c917
Phi Hoang <hoangphi.dev@gmail.com>
2020-08-04 22:16:11 +0700

    update 3 array

--- array.php
+++ array.php
@@ -6,5 +6,22 @@
 	array_push($data, $data2);
 	echo $data[0];
 	print_r($data);
-	
+
+	// Index array - mảng đánh theo kiểu số
+	$arr_index = array("Phi", "Hoang", "Mr");
+	echo $arr_index[0];
+	// Associative - mảng được đính key cụ thể
+	$arr_assoc = array(
+		"ho" => "Hoang",
+		"ten" => "Phi",
+		"sdt" => "0989324221"
+	);
+	echo $arr_assoc['ho'];
+	// Multidimensional arrays
+	$arr_multi = array(
+		array(
+			"Frog",
+			"Dog"
+		)
+	);
  ?>
